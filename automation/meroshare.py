1942ee4aea81b78e0bc50c0bf66e93dffbc570aa
Nissan Sherpa <pemshera@gmail.com>
2024-06-25 20:19:18 +0545

    scraping shares data in portfolio from mero share

--- automation/meroshare.py
+++ automation/meroshare.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import openpyxl
 import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -35,5 +37,20 @@ time.sleep(1)
 
 btn_port = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="sideBar"]/nav/ul/li[5]/a')))
 btn_port.click()
-time.sleep(10)
+time.sleep(5)
+
+table = driver.find_element(By.XPATH, '//*[@id="main"]/div/app-my-portfolio/div/div[2]/div/div/table')
+rows = table.find_elements(By.TAG_NAME, "tr")
+
+all_data = []
+for row in rows[1:-1]:
+    cells = row.find_elements(By.TAG_NAME, "td")
+    data = {'Name':cells[1].text, 'Price':cells[5].text}
+    print(cells[1].text, cells[5].text)
+    all_data.append(data)
+print(all_data)
+
+df = pd.DataFrame(all_data)
+df.to_excel("portfolio.xlsx")
+
 driver.quit()
